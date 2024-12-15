e5f18da2315f502b9e8333e42aa227337494e8ec
Reful4ent <dima2323fr3@gmail.com>
2024-12-15 15:50:35 +0700

    fix: statement route and controller

--- back-admin/src/statement/controllers/statement.controller.js
+++ back-admin/src/statement/controllers/statement.controller.js
@@ -4,14 +4,16 @@ class StatementController {
     async createStatement(req, res) {
         const dataFromRequest = req.body ?? {};
 
+        console.log(dataFromRequest);
+
         try {
-            if (dataFromRequest.number_of_invoice &&
+            if (dataFromRequest.number_of_statement &&
                 dataFromRequest.receipt_date &&
                 dataFromRequest.supplier_id &&
-                dataFromRequest.total_sum) {
+                dataFromRequest.total_sum >=0 ) {
 
                 const newStatement = await db.query(`INSERT INTO statements (number, receipt_date, supplier_id, total_sum) 
-                                                    VALUES ('${dataFromRequest.number_of_invoice}',
+                                                    VALUES ('${dataFromRequest.number_of_statement}',
                                                             '${dataFromRequest.receipt_date}',
                                                             ${dataFromRequest.supplier_id},
                                                             ${dataFromRequest.total_sum})
@@ -27,19 +29,30 @@ class StatementController {
     }
 
     async getStatements(req, res) {
-        let requestToDB = `SELECT * FROM statements 
-                                  WHERE ($1::text IS NULL OR number ILIKE $1) 
-                                  AND ($2::date IS NULL OR receipt_date::date = $2::date)
-                                  AND ($3::int IS NULL OR supplier_id = $3) 
-                                  AND ($4::decimal IS NULL OR total_sum = $4)
+        let requestToDB = `SELECT
+                                        statements.id,
+                                        statements.number,
+                                        statements.receipt_date,
+                                        statements.total_sum,
+                                        supplier.id AS supplier_id,
+                                        supplier.name AS supplier_name
+                                  FROM 
+                                        statements 
+                                  JOIN
+                                        supplier ON statements.supplier_id = supplier.id
+                                  WHERE 
+                                        ($1::text IS NULL OR number ILIKE $1) 
+                                        AND ($2::date IS NULL OR receipt_date::date = $2::date)
+                                        AND ($3::text IS NULL OR supplier.name ILIKE $3) 
+                                        AND ($4::decimal IS NULL OR total_sum = $4);
         `;
 
-        const { number, receipt_date, supplier_id, total_sum } = req.body ?? {};
+        const { number, receipt_date, supplier_name, total_sum } = req.body ?? {};
 
         const values = [
             number ? `%${number}%` : null,
             receipt_date,
-            supplier_id,
+            supplier_name ? `%${supplier_name}%` : null,
             total_sum,
         ]
 
@@ -56,7 +69,18 @@ class StatementController {
         const id = req.params.id
 
         try {
-            const statement = await db.query(`SELECT * FROM statements WHERE id = ${id}`);
+            const statement = await db.query(`SELECT
+                                                    statements.id,
+                                                    statements.number,
+                                                    statements.receipt_date,
+                                                    statements.total_sum,
+                                                    supplier.id AS supplier_id,
+                                                    supplier.name AS supplier_name
+                                              FROM 
+                                                    statements 
+                                              JOIN
+                                                    supplier ON statements.supplier_id = supplier.id
+                                              WHERE statements.id = ${id}`);
             res.status(200).json(statement.rows[0]);
         } catch (error) {
             res.status(500).json({ error: error.message });
@@ -68,18 +92,18 @@ class StatementController {
         const dataFromRequest = req.body ?? {};
 
         try {
-            if(dataFromRequest.number_of_invoice &&
+            if(dataFromRequest.number_of_statement &&
                 dataFromRequest.receipt_date &&
                 dataFromRequest.supplier_id &&
-                dataFromRequest.total_sum) {
+                dataFromRequest.total_sum >= 0) {
 
                 const statement = await db.query(`UPDATE statements 
-                                                 SET number='${dataFromRequest.number_of_invoice}', 
+                                                 SET number='${dataFromRequest.number_of_statement}', 
                                                      receipt_date='${dataFromRequest.receipt_date}', 
                                                      supplier_id=${dataFromRequest.supplier_id}, 
                                                      total_sum=${dataFromRequest.total_sum}
                                                  WHERE id=${id} 
-                                                 RETURNING *'`)
+                                                 RETURNING *`)
                 res.status(200).json(statement.rows[0]);
             } else {
                 res.status(400).json({ error: "Bad request" });
--- back-admin/src/statement/routes/statement.routes.js
+++ back-admin/src/statement/routes/statement.routes.js
@@ -2,10 +2,10 @@ const Router = require('express');
 const router = new Router();
 const statementController = require('../controllers/statement.controller');
 
-router.post('/statements', statementController.createStatement)
+router.post('/statements/create', statementController.createStatement)
 router.get('/statements', statementController.getStatements)
 router.get('/statements/:id', statementController.getOneStatement)
 router.put('/statements/:id', statementController.updateStatement)
-router.delete('statements/:id', statementController.deleteStatement)
+router.delete('/statements/:id', statementController.deleteStatement)
 
 module.exports = router;
