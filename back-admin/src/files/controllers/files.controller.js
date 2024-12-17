4e67824d338c65268531948856186df404448543
Reful4ent <dima2323fr3@gmail.com>
2024-12-17 18:37:33 +0700

    finally all with back

--- back-admin/src/files/controllers/files.controller.js
+++ back-admin/src/files/controllers/files.controller.js
@@ -1,49 +1,166 @@
 const db = require('../../../db')
 const excelJS = require("exceljs");
+const fs = require('fs');
+const path = require('path');
+const { Document, Packer, Table, TableRow, TableCell, TextRun, Paragraph} = require('docx');
 
-const User = [
-    {
-        fname: "John",
-        lname: "Doe",
-        email: "john.doe@example.com",
-        gender: "Male",
-    },
-    {
-        fname: "Jane",
-        lname: "Doe",
-        email: "jane.doe@example.com",
-        gender: "Female",
-    },
-    {
-        fname: "Bob",
-        lname: "Smith",
-        email: "bob.smith@example.com",
-        gender: "Male",
-    },
-];
-
+const CONCRETE_NAMES = ['invoice', 'statements']
 
 class FilesController {
     async createWord(req, res) {
-        const dataFromRequest = req.body ?? {};
+        const nameDb = req.params.name ?? '';
+        let responseDb;
+        try {
+            if(nameDb === 'invoice') {
+                responseDb = await db.query(`SELECT 
+                                                invoice.number AS "Номер", 
+                                                TO_CHAR(invoice.discharge_date, 'YYYY-MM-DD') AS "Дата выписки", 
+                                                employee.surname AS "Фамилия сотрудника",
+                                                buyer.name AS "Покупатель",
+                                                medicine.name AS "Лекарство", 
+                                                TO_CHAR(medicine.production_date, 'YYYY-MM-DD') AS "Дата производства", 
+                                                medicine.registration_num AS "Регистрационный номер", 
+                                                TO_CHAR(medicine.expiration_date, 'YYYY-MM-DD') AS "Срок годности", 
+                                                invoice_medicine.price_that_time AS "Цена на момент покупки", 
+                                                invoice_medicine.quantity AS "Количество", 
+                                                invoice.total_sum AS "Итоговая цена"
+                                            FROM 
+                                                invoice
+                                            JOIN 
+                                                invoice_medicine ON invoice.id = invoice_medicine.invoice_id
+                                            JOIN 
+                                                employee ON invoice.employee_id = employee.id
+                                            JOIN
+                                                buyer ON invoice.buyer_id = buyer.id
+                                            JOIN 
+                                                medicine ON invoice_medicine.medicine_id = medicine.id;`);
+            } else if (nameDb === 'statements') {
+                responseDb = await db.query(`SELECT 
+                                                statements.number AS "Номер", 
+                                                statements.receipt_date AS "Дата получения", 
+                                                medicine.name AS "Лекарство", 
+                                                medicine.production_date AS "Дата производства", 
+                                                medicine.registration_num AS "Регистрационный номер", 
+                                                medicine.expiration_date AS "Срок годности", 
+                                                statements_medicine.price_that_time AS "Цена на момент покупки", 
+                                                statements_medicine.quantity AS "Количество", 
+                                                statements.total_sum AS "Итоговая цена"
+                                            FROM 
+                                                statements
+                                            JOIN 
+                                                statements_medicine ON statements.id = statements_medicine.statements_id
+                                            JOIN 
+                                                medicine ON statements_medicine.medicine_id = medicine.id;`);
+            } else {
+                responseDb = await db.query(`SELECT * FROM ${nameDb}`);
+            }
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+        const columns = Object.keys(responseDb.rows[0])
+
+        const rows = responseDb.rows.map(row => {
+            return new TableRow({
+                children: columns.map(column => {
+                    return new TableCell({
+                        children: [new Paragraph(row[column]?.toString() || '')],
+                    });
+                }),
+            });
+        });
+
+        const headerRow = new TableRow({
+            children: columns.map(column => {
+                return new TableCell({
+                    children: [new Paragraph(column)],
+                });
+            }),
+        });
+
+        const table = new Table({
+            rows: [headerRow, ...rows],
+            columnWidths: Array(columns.length).fill(2000), // Установка ширины колонок
+        });
+
+
+        const doc = new Document({
+            sections: [{
+                children: [table],
+            }],
+        });
+
+        const b64string = await Packer.toBase64String(doc);
+
+        res.setHeader('Content-Disposition', `attachment; filename=${nameDb}.docx`);
+        res.send(Buffer.from(b64string, 'base64'));
     }
 
     async createExcel(req, res) {
-        const dataFromRequest = req.body ?? {};
-        console.log(req.params.name);
+        const nameDb = req.params.name ?? '';
+
+        let responseDb;
+        try {
+            if(nameDb === 'invoice') {
+                responseDb = await db.query(`SELECT 
+                                                invoice.number AS "Номер", 
+                                                TO_CHAR(invoice.discharge_date, 'YYYY-MM-DD') AS "Дата выписки", 
+                                                employee.surname AS "Фамилия сотрудника",
+                                                buyer.name AS "Покупатель",
+                                                medicine.name AS "Лекарство", 
+                                                TO_CHAR(medicine.production_date, 'YYYY-MM-DD') AS "Дата производства", 
+                                                medicine.registration_num AS "Регистрационный номер", 
+                                                TO_CHAR(medicine.expiration_date, 'YYYY-MM-DD') AS "Срок годности", 
+                                                invoice_medicine.price_that_time AS "Цена на момент покупки", 
+                                                invoice_medicine.quantity AS "Количество", 
+                                                invoice.total_sum AS "Итоговая цена"
+                                            FROM 
+                                                invoice
+                                            JOIN 
+                                                invoice_medicine ON invoice.id = invoice_medicine.invoice_id
+                                            JOIN 
+                                                employee ON invoice.employee_id = employee.id
+                                            JOIN
+                                                buyer ON invoice.buyer_id = buyer.id
+                                            JOIN 
+                                                medicine ON invoice_medicine.medicine_id = medicine.id;`);
+            } else if (nameDb === 'statements') {
+                responseDb = await db.query(`SELECT 
+                                                statements.number AS "Номер", 
+                                                statements.receipt_date AS "Дата получения", 
+                                                medicine.name AS "Лекарство", 
+                                                medicine.production_date AS "Дата производства", 
+                                                medicine.registration_num AS "Регистрационный номер", 
+                                                medicine.expiration_date AS "Срок годности", 
+                                                statements_medicine.price_that_time AS "Цена на момент покупки", 
+                                                statements_medicine.quantity AS "Количество", 
+                                                statements.total_sum AS "Итоговая цена"
+                                            FROM 
+                                                statements
+                                            JOIN 
+                                                statements_medicine ON statements.id = statements_medicine.statements_id
+                                            JOIN 
+                                                medicine ON statements_medicine.medicine_id = medicine.id;`);
+            } else {
+                responseDb = await db.query(`SELECT * FROM ${nameDb}`);
+            }
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+
         const workbook = new excelJS.Workbook();
-        const worksheet = workbook.addWorksheet("Users");
+        const worksheet = workbook.addWorksheet(`${nameDb}`);
+        const columns = Object.keys(responseDb.rows[0]).map(columnName => ({header: columnName, key: columnName, width: 30}));
 
-        worksheet.columns = [
-            { header: "First Name", key: "fname", width: 15 },
-            { header: "Last Name", key: "lname", width: 15 },
-            { header: "Email", key: "email", width: 25 },
-            { header: "Gender", key: "gender", width: 10 },
-        ];
+        worksheet.columns = columns;
+        responseDb.rows.forEach(response => {
+            const row = worksheet.addRow(response);
+            row.eachCell(cell => {
+                cell.alignment = { vertical: 'top', horizontal: 'left' };
+            });
+        });
 
-        User.forEach(user => { worksheet.addRow(user); });
 
-        res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"); res.setHeader("Content-Disposition", "attachment; filename=" + "users.xlsx");
+        res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"); res.setHeader("Content-Disposition", "attachment; filename=" + `${nameDb}.xlsx`);
 
         workbook.xlsx.write(res).then(() => res.end());
     }
--- back-admin/src/files/routes/files.routes.js
+++ back-admin/src/files/routes/files.routes.js
@@ -2,7 +2,7 @@ const Router = require('express');
 const router = new Router();
 const filesController = require('../controllers/files.controller');
 
-router.get('/word/create', filesController.createWord)
+router.get('/word/create/:name', filesController.createWord)
 router.get('/excel/create/:name', filesController.createExcel)
 
 module.exports = router;
