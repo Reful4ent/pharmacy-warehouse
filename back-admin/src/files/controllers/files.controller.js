b473db260d273cc658c08ce0c8da8f2348e79740
Reful4ent <dima2323fr3@gmail.com>
2024-12-19 20:22:52 +0700

    Ворд редактирование

--- back-admin/src/files/controllers/files.controller.js
+++ back-admin/src/files/controllers/files.controller.js
@@ -20,6 +20,8 @@ class FilesController {
                                                 TO_CHAR(invoice.discharge_date, 'YYYY-MM-DD') AS "Дата выписки", 
                                                 employee.surname AS "Фамилия сотрудника",
                                                 buyer.name AS "Покупатель",
+                                                buyer.tin AS "ИНН",
+                                                street.name AS "Улица",
                                                 medicine.name AS "Лекарство", 
                                                 TO_CHAR(medicine.production_date, 'YYYY-MM-DD') AS "Дата производства", 
                                                 medicine.registration_num AS "Регистрационный номер", 
@@ -35,6 +37,8 @@ class FilesController {
                                                 employee ON invoice.employee_id = employee.id
                                             JOIN
                                                 buyer ON invoice.buyer_id = buyer.id
+                                            JOIN
+                                                street ON buyer.street_id = street.id
                                             JOIN 
                                                 medicine ON invoice_medicine.medicine_id = medicine.id
                                             WHERE ($1::text IS NULL OR number ILIKE $1)`,[`%${number}%`]);
@@ -61,7 +65,9 @@ class FilesController {
         } catch (error) {
             res.status(500).json({ error: error.message });
         }
-        const columns = Object.keys(responseDb.rows[0])
+
+        let columns = Object.keys(responseDb.rows[0]);
+        columns = columns.filter((_, index) => index > 5 && index < columns.length - 1);
 
         const rows = responseDb.rows.map(row => {
             return new TableRow({
@@ -81,16 +87,387 @@ class FilesController {
             }),
         });
 
-        const table = new Table({
-            rows: [headerRow, ...rows],
-            columnWidths: Array(columns.length).fill(2000), // Установка ширины колонок
+        const footerRow = new TableRow({
+                children: [
+                    new TableCell({
+                        children: [new Paragraph("Всего к оплате:")],
+                    }),
+                    new TableCell({
+                        children: [new Paragraph(`${responseDb.rows[0]['Итоговая цена']}`)],
+                    }),
+                ],
+        })
+
+        const dataTable = new Table({
+            rows: [headerRow, ...rows, footerRow],
+            width: {
+                size: 100,
+                type: "pct",
+            },
         });
 
+        // Заголовок документа
+        const header = new Paragraph({
+            children: [
+                new TextRun({
+                    text: `СЧЕТ-ФАКТУРА №${responseDb.rows[0]['Номер']} oт ${responseDb.rows[0]['Дата выписки']}`,
+                    bold: true,
+                    size: 28,
+                }),
+            ],
+            alignment: "center",
+        });
+        const header_2 = new Paragraph({
+            children: [
+                new TextRun({
+                    text: `ИСПРАВЛЕНИЕ № ___________ oт __________ `,
+                    bold: true,
+                    size: 28,
+                }),
+            ],
+            alignment: "center",
+        });
 
+        const fields = [
+            "Продавец: Аптечный склад №1",
+            "Адрес: Улица Баумана д31",
+            "ИНН/КПП продавца: 511833352511",
+            "Грузоотправитель и его адрес: CДЕК, Пахомово д45",
+            "Грузополучатель и его адрес: СДЕК, Блюхера д123",
+            `К платежно-расчетному документу №: ${Math.floor(1 + Math.random() * (5000 + 1 - 1))}`,
+            `Покупатель: ${responseDb.rows[0]['Покупатель']}`,
+            `Адрес: г.Новосибирск ул.${responseDb.rows[0]['Улица']} д.${Math.floor(1 + Math.random() * (40 + 1 - 1))}`,
+            `ИНН/КПП покупателя: ${responseDb.rows[0]['ИНН']}`,
+            "Валюта: Рубли",
+        ].map(field => new Paragraph(field));
+
+        // Итоговая таблица
+
+        const footerTable = new Table({
+            rows: [
+                new TableRow({
+                    children: [
+                        new TableCell({
+                            children: [new Paragraph("Руководитель организации или иное уполномоченное лицо")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "single" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                            width: {
+                                size: 20,  // Ширина в процентах
+                                type: "pct",
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "single" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                            width: {
+                                size: 20,  // Ширина в процентах
+                                type: "pct",
+                            },
+                        }),
+                    ],
+                    margins: {
+                        top: 20,
+                        bottom: 20,
+                        left: 0,
+                        right: 0,
+                    },
+                }),
+                new TableRow({
+                    children: [
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                            width: {
+                                size: 20,  // Ширина в процентах
+                                type: "pct",
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                            width: {
+                                size: 20,  // Ширина в процентах
+                                type: "pct",
+                            },
+                        }),
+                    ],
+                }),
+                new TableRow({
+                    children: [
+                        new TableCell({
+                            children: [new Paragraph("Главный бухгалтер или иное уполномоченное лицо")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "single" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                            width: {
+                                size: 20,  // Ширина в процентах
+                                type: "pct",
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "single" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                            width: {
+                                size: 20,  // Ширина в процентах
+                                type: "pct",
+                            },
+                        }),
+                    ],
+                }),
+                new TableRow({
+                    children: [
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                            width: {
+                                size: 20,  // Ширина в процентах
+                                type: "pct",
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                            width: {
+                                size: 20,  // Ширина в процентах
+                                type: "pct",
+                            },
+                        }),
+                    ],
+                }),
+                new TableRow({
+                    children: [
+                        new TableCell({
+                            children: [new Paragraph("Индивидуальный предприниматель или иное уполномоченное лицо")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "single" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                            width: {
+                                size: 20,  // Ширина в процентах
+                                type: "pct",
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "none" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                        }),
+                        new TableCell({
+                            children: [new Paragraph("")],
+                            borders: {
+                                top: { style: "none" },
+                                bottom: { style: "single" },
+                                left: { style: "none" },
+                                right: { style: "none" },
+                            },
+                            width: {
+                                size: 20,  // Ширина в процентах
+                                type: "pct",
+                            },
+                        }),
+                    ],
+                }),
+            ],
+            width: {
+                size: 100,
+                type: "pct",
+            },
+            borders: {
+                top: { style: "none" },
+                bottom: { style: "none" },
+                left: { style: "none" },
+                right: { style: "none" },
+            },
+        });
+
+        // Создание документа
         const doc = new Document({
-            sections: [{
-                children: [table],
-            }],
+            sections: [
+                {
+                    children: [
+                        header,
+                        header_2,
+                        new Paragraph(" "),
+                        ...fields,
+                        new Paragraph(" "),
+                        new Paragraph(" "),
+                        dataTable,
+                        new Paragraph(" "),
+                        new Paragraph(" "),
+                        footerTable,
+                    ],
+                },
+            ],
         });
 
         const b64string = await Packer.toBase64String(doc);
